6bfb929d40be018af8a381225dd6741bd432e5cf
Matheus Muriel <matheus.muriel@outlook.com>
2019-11-12 12:06:47 -0300

    :wrench: Adicionado Hibernate

--- src/br/edu/unifil/servlet/ContatoServlet.java
+++ src/br/edu/unifil/servlet/ContatoServlet.java
@@ -1,9 +1,14 @@
 package br.edu.unifil.servlet;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,6 +16,10 @@ public class ContatoServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("lab3-jsp");
+
+        //EntityManager em = emf.createEntityManager();
 
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
